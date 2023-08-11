544b0cc50a6c4071de3af4d94006269aff3d5983
4nth0ny1 <acatullo4@gmail.com>
2023-08-11 17:31:02 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+// https://jsonplaceholder.typicode.com/posts
+
+function App() {
+  return (
+    <>
+      <h1>React Query Tutorial Josh </h1>
+    </>
+  );
+}
+
+export default App;
